7b88ad8a795eb76414196ec8e62f1d2d16d1e69b
Christian Foellmann <foellmann@foe-services.de>
2015-01-26 15:46:48 +0100

    add scm-update.sh

---
+++ scm-update.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Update all Git and Subversion repositories under the current directory.
+# by Mike McQuaid (https://github.com/mikemcquaid/scripts)
+
+CURRENT=$PWD
+# Set to newline to loop over find output correctly on spaced paths.
+IFS="
+"
+
+echorun() {
+  echo + "$@"
+  $*
+}
+
+for SCM in $(find -L $CURRENT -name .git) $(find $CURRENT -name .svn)
+do
+  DIRECTORY=$(dirname $SCM)
+  cd $DIRECTORY
+  if [ -d ../.svn ] || echo $DIRECTORY | grep -q "vendor/ruby"
+  then
+    continue
+  fi
+  echo == Updating $(basename $DIRECTORY)
+  if [ -d .git/svn ]
+  then
+    echorun git svn fetch
+    echorun git svn rebase
+  elif [ -d .git ]
+  then
+    echorun git fetch --all
+    [ -n "$(git remote -v)" ] && echorun git pull
+  elif [ -d .svn ]
+  then
+    echorun svn update
+  fi
+  echo
+done
