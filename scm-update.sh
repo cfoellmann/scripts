d39126a9b7821f2ee0c65671a451a9a8a1a5cda1
Christian Foellmann <foellmann@foe-services.de>
2015-01-26 15:47:00 +0100

    windows fix for scm-update.sh

--- scm-update.sh
+++ scm-update.sh
@@ -12,7 +12,7 @@ echorun() {
   $*
 }
 
-for SCM in $(find -L $CURRENT -name .git) $(find $CURRENT -name .svn)
+for SCM in $(find $CURRENT -name .git) $(find $CURRENT -name .svn)
 do
   DIRECTORY=$(dirname $SCM)
   cd $DIRECTORY
